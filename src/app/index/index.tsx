0c6ccb5bbcc280f5886f1c9827fd0767e9c076ac
Rodrigo <rwesley2000@gmail.com>
2025-09-21 21:56:27 -0300

    first-commit

---
+++ src/app/index/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import { styles } from './style';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>hello word</Text>
+    </View>
+    
+  );
+}
---
+++ src/app/index/style.ts
@@ -0,0 +1,11 @@
+import { colors } from '@/styles/colors';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.gray[900],
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
