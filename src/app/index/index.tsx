1f849aba1df514aad5b6dba487727d36b0d3bf34
Rodrigo <rwesley2000@gmail.com>
2025-09-25 22:28:01 -0300

    feat: creating List

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,7 +1,10 @@
 import { colors } from '@/styles/colors';
+import { categories } from '@/utils/catogories';
 import { MaterialIcons } from '@expo/vector-icons';
 import { Image, TouchableOpacity, View } from 'react-native';
 import { styles } from './style';
+
+import { Category } from '@/components/category';
 
 export default function Index() {
   return (
@@ -14,6 +17,13 @@ export default function Index() {
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
+
+      <Category 
+        name={categories[0].name}
+        icon={categories[0].icon}
+        isSelected/>
+      <Category name='Site' icon='language'isSelected={false}/>
+      <Category name='Video' icon='movie' isSelected={false}/>
     </View>
   );
 }
