998711efddc5a9eb83a8434d522a4319b6ea16c9
Rodrigo <rwesley2000@gmail.com>
2025-09-28 21:39:16 -0300

    feat: storage, function remove

--- src/storage/link-storage.ts
+++ src/storage/link-storage.ts
@@ -28,4 +28,16 @@ async function save(newLink: LinkStorage) {
     }
 }
 
+async function remove(id: string){
+    try {
+        const storage = await get()
+
+        const upadate = storage.filter((link) => link.id !== id)
+
+        await AsyncStorage.setItem(LINKS_STORAGE_KEY, JSON.stringify(upadate))
+    } catch (error) {
+        throw error
+    }
+}
+
 export const linkStorage = {get, save}
